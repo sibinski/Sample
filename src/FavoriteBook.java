7db6de0212734796cb0d97c38f54db499eaa8569
Jevgeni Shibinski <jevgeni.shibinski@gmail.com>
2025-03-18 12:36:21 +0200

    My favorite book and details about its price.

---
+++ src/FavoriteBook.java
@@ -0,0 +1,9 @@
+import static java.lang.System.out;
+
+public static void main(String[] args) {
+    String booksTitle = "Dracula";
+    String authorsName = "Bram Stoker";
+    int numberOfPages = 223;
+    float price = 19.99f;
+    out.println("My favorite book is: " + booksTitle + ". \nIt's author is: " + authorsName + ". \nIt consist of " + numberOfPages + " pages. \nIt's price is: $" + price + ". \nI love it!");
+}
