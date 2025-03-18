99b4a27a46b928ef7fc47846f97c6507546650fe
Jevgeni Shibinski <jevgeni.shibinski@gmail.com>
2025-03-18 12:16:35 +0200

    Updated data for a car.

--- src/CarInfo.java
+++ src/CarInfo.java
@@ -1,2 +1,14 @@
-public class CarInfo {
+import static java.lang.System.out;
+
+public static void main(String[] args) {
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2022;
+    double milleage = 15002.5;
+    boolean isAutomatic = true;
+    out.println("Car make: " + make);
+    out.println("Car model: " + model);
+    out.println("Year of manufacturing: " + year);
+    out.println("Milleage: " + milleage);
+    out.println("Automatic transmission: " + isAutomatic);
 }
